7c8aac0016656cab76d73c66079fdbb464a04833
IgnoranteSl <51123884+IgnoranteSl@users.noreply.github.com>
2019-06-20 18:12:07 -0300

    Create DiscordCommand.js

---
+++ src/structure/DiscordCommand.js
@@ -0,0 +1,16 @@
+module.exports = class Command {
+    constructor(client) {
+        
+        this.client = client
+        
+        this.name = 'commandName'
+        this.category = 'Category'
+        this.description = 'Not description'
+        this.aliases = []
+
+        this.enabled = true
+        this.guildOnly = false
+    }
+
+    async run() {}
+}
